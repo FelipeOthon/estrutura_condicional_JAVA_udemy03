52429c575aae2c90f3df22c80f01b032d33f1f0c
FelipeOthon <felipeosantiago@gmail.com>
2022-10-23 13:24:58 -0300

    enviar exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Digite um numero");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+
+            System.out.println("Numero Par");
+        } else {
+
+            System.out.println("Numero Impar");
+        }
+        scanner.close();
+
     }
 }
